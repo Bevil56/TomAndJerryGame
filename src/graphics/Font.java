e1a711fb5c4151b0b0a3a0af7fa81326e8a2c6df
Bevil56 <thanhvinhll56@gmail.com>
2023-12-21 12:21:09 +0700

    Adding loading Font & Sprite class

---
+++ src/graphics/Font.java
@@ -0,0 +1,102 @@
+package graphics;
+
+import java.awt.image.BufferedImage;
+import java.util.Objects;
+
+import javax.imageio.ImageIO;
+
+public class Font {
+
+    private BufferedImage FONTSHEET = null;
+    private BufferedImage[][] spriteArray;
+    private final int TILE_SIZE = 32;
+    public int width;
+    public int heigth;
+    private int letterWidth;
+    private int letterHeight;
+
+    public Font(String file) {
+        width = TILE_SIZE;
+        heigth = TILE_SIZE;
+
+        System.out.println("Loading: " + file + "...");
+        FONTSHEET = loadFont(file);
+
+        letterWidth = FONTSHEET.getWidth() / width;
+        letterHeight = FONTSHEET.getHeight() / heigth;
+        loadFontArray();
+    }
+
+    public Font(String file, int w, int h) {
+        this.width = w;
+        this.heigth = h;
+
+        System.out.println("Loading: " + file + "...");
+        FONTSHEET = loadFont(file);
+
+        letterWidth = FONTSHEET.getWidth() / w;
+        letterHeight = FONTSHEET.getHeight() / h;
+        loadFontArray();
+    }
+
+    public void setSize(int width, int height) {
+        setWidth(width);
+        setHeight(height);
+    }
+
+    public void setWidth(int i) {
+        width = i;
+        letterWidth = FONTSHEET.getWidth() / width;
+    }
+
+    public void setHeight(int i) {
+        heigth = i;
+        letterHeight = FONTSHEET.getHeight() / heigth;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return heigth;
+    }
+
+    private BufferedImage loadFont(String file) {
+        BufferedImage sprite = null;
+        try {
+            sprite = ImageIO.read(Objects.requireNonNull(getClass().getClassLoader().getResourceAsStream(file)));
+        } catch (Exception e) {
+            System.out.println("ERROR: could not load file: " + file);
+        }
+
+        return sprite;
+    }
+
+    public void loadFontArray() {
+        spriteArray = new BufferedImage[letterWidth][letterHeight];
+
+        for (int x = 0; x < letterWidth; x++) {
+            for (int y = 0; y < letterHeight; y++) {
+                spriteArray[x][y] = getLetter(x, y);
+            }
+        }
+    }
+
+    public BufferedImage getFontSheet() {
+        return FONTSHEET;
+    }
+
+    public BufferedImage getLetter(int x, int y) {
+        BufferedImage img = FONTSHEET.getSubimage(x * width, y * heigth, width, heigth);
+        return img;
+    }
+
+    public BufferedImage getLetter(char letter) {
+        int value = letter ;
+
+        int x = value % letterWidth;
+        int y = value / letterWidth;
+        return getLetter(x, y);
+    }
+}
---
+++ src/graphics/Sprite.java
@@ -0,0 +1,123 @@
+package graphics;
+
+import math.Vector2f;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class Spirte {
+    private BufferedImage spriteSheet = null;
+    private BufferedImage[][] spriteArray;
+    private final int TILE_SIZE = 32;
+    private int width;
+    private int height;
+    private int spriteWidth;
+    private int spriteHeight;
+    public Spirte(String file){
+        width = TILE_SIZE;
+        height = TILE_SIZE;
+
+        System.out.println("Loading: " + file +"...");
+
+        spriteSheet = loadSpriteSheet(file);
+
+        spriteWidth = spriteSheet.getWidth()/width;
+        spriteHeight = spriteSheet.getHeight()/height;
+
+        loadSpriteArray();
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public Spirte(String file, int width, int height){
+        this.width = width;
+        this.height = height;
+
+        System.out.println("Loading: " + file +"...");
+
+        spriteSheet = loadSpriteSheet(file);
+
+        spriteWidth = spriteSheet.getWidth()/width;
+        spriteHeight = spriteSheet.getHeight()/height;
+
+        loadSpriteArray();
+    }
+
+    public void setSize(int width,int height) {
+        setWidth(width);
+        setHeight(height);
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+        spriteHeight = spriteSheet.getHeight()/height;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+        spriteWidth = spriteSheet.getWidth()/width;
+    }
+
+    private BufferedImage loadSpriteSheet(String file) {
+        BufferedImage sprite = null;
+        try{
+            sprite = ImageIO.read(getClass().getClassLoader().getResourceAsStream(file));
+        }catch (Exception e){
+            System.out.println("ERROR: could not load file: " + file);
+        }
+        return sprite;
+    }
+    public void loadSpriteArray() {
+        spriteArray = new BufferedImage[spriteWidth][spriteHeight];
+        for (int x = 0 ; x < spriteWidth ; x++){
+            for (int y = 0 ; y < spriteHeight ; y++){
+                spriteArray[x][y] = getSprite(x,y);
+            }
+        }
+    }
+    public BufferedImage getSpriteSheet(){
+        return spriteSheet;
+    }
+    public BufferedImage getSprite(int x,int y){
+        return spriteSheet.getSubimage(x * width, y * height , width , height);
+    }
+    public  BufferedImage[] getSpriteArray(int i){
+        return spriteArray[i];
+    }
+    public BufferedImage[][] getSpriteArray2D(int i){
+        return spriteArray;
+    }
+    public static void drawSprite(Graphics2D g2D, ArrayList<BufferedImage>  img, Vector2f pos, int width, int height, int xOffset, int yOffset ){
+        float x = pos.x;
+        float y = pos.y;
+
+        for (int i = 0; i < img.size();  i++){
+            if(img.get(i) != null){
+                g2D.drawImage(img.get(i), (int) x, (int) y, width, height, null );
+            }
+            x += xOffset;
+            y += yOffset;
+        }
+    }
+    public static void drawSprite(Graphics2D g2D, Font font, String word, Vector2f pos, int width, int height, int xOffset, int yOffset ){
+        float x = pos.x;
+        float y = pos.y;
+        for (int i = 0; i < word.length();  i++){
+            if(word.charAt(i) != 32){
+                g2D.drawImage(font.getFont(word.charAt(i)), (int) x, (int) y, width, height, null );
+            }
+            x += xOffset;
+            y += yOffset;
+        }
+    }
+}
+
